85a82442717f96cb3d5b801a0734954b54e7abfc
Roman.Pererushev <roman.pererushev@gmail.com>
2024-09-13 10:18:44 +0300

    Transactions

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Controllers\{HomeController, AboutController, AuthController};
+use App\Controllers\{HomeController, AboutController, AuthController, TransactionController};
 use App\Middleware\{AuthRequiredMiddleware, GuestOnlyMiddleware};
 
 
@@ -18,4 +18,6 @@ function registerRoutes(App $app)
     $app->post('/register', [AuthController::class, 'register'])->add(GuestOnlyMiddleware::class);
     $app->post('/login', [AuthController::class, 'login'])->add(GuestOnlyMiddleware::class);
     $app->get('/logout', [AuthController::class, 'logout'])->add(AuthRequiredMiddleware::class);
+    $app->get('/transaction', [TransactionController::class, 'createView'])->add(AuthRequiredMiddleware::class);
+    $app->post('/transaction', [TransactionController::class, 'create'])->add(AuthRequiredMiddleware::class);
 }
